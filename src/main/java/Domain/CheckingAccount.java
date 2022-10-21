f559dd3bfaafd7f0d2173126adccf9a75b9933e5
thiago <ts_muniz@outlook.com>
2022-10-21 14:22:51 -0300

    fix: adiciona metodos na classe checkingAccount

--- src/main/java/Domain/CheckingAccount.java
+++ src/main/java/Domain/CheckingAccount.java
@@ -4,5 +4,11 @@ import java.time.LocalDate;
 
 public class CheckingAccount extends Account{
 
+  private static int sequencial = 1;
+  public CheckingAccount(Domain.Customer customer, Double balance) {
+    super.accountNumber = sequencial++;
+    super.customer = customer;
+    super.balance = balance;
 
+  }
 }
