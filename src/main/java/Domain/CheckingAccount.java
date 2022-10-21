350c788720a1c0fd5bd02bf3f7926c6e09f81887
thiago <ts_muniz@outlook.com>
2022-10-21 10:40:37 -0300

    feat: adiciona atributos da classe Custoomer

--- src/main/java/Domain/CheckingAccount.java
+++ src/main/java/Domain/CheckingAccount.java
@@ -1,4 +1,17 @@
 package Domain;
 
+import java.time.LocalDate;
+
 public class CheckingAccount extends Account{
+  private String name;
+  private String profession;
+  private LocalDate bornDate;
+  private Account account;
+
+  public CheckingAccount(String name, String profession, LocalDate bornDate, Account account) {
+    this.name = name;
+    this.profession = profession;
+    this.bornDate = bornDate;
+    this.account = account;
+  }
 }
